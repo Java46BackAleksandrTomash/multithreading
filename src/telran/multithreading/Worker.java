b4db354e95c94795b2d9d808e0c247ad3e0c87d0
aleksandrtomash <tombasket508@gmail.com>
2022-11-27 17:08:44 +0200

    HW #42 Deadlock application

---
+++ src/telran/multithreading/Worker.java
@@ -0,0 +1,48 @@
+package telran.multithreading;
+
+public class Worker extends Thread {
+
+	static Object resource1 = new Object();
+	static Object resource2 = new Object();
+	static Object resource3 = new Object();
+	static int nRUNS = 1;
+
+	void f1() {
+		synchronized (resource1) {
+			try {
+				sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			synchronized (resource2) {
+				synchronized (resource3) {
+					// any code
+				}
+			}
+		}
+	}
+
+	void f2() {
+		synchronized (resource3) {
+			synchronized (resource1) {
+				// any code
+			}
+		}
+	}
+	void f3() {
+		synchronized (resource2) {
+			synchronized (resource3) {
+				// any code
+			}
+		}
+	}
+	@Override
+	public void run() {
+		for (int i = 0; i < nRUNS; i++) {
+			f1();
+			f2();
+			f3();
+		}
+	}
+}
